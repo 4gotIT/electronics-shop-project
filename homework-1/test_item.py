d5833dbfea18b2ebbcd765527dfd337025ad1ebe
Aziz <ibodov.aziz@yandex.ru>
2023-11-07 12:47:08 +0300

    fix test, cov == 100%

--- homework-1/test_item.py
+++ homework-1/test_item.py
@@ -1,18 +1,20 @@
 from src.item import Item
+import pytest
 
-if __name__ == '__main__':
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
 
-    print(item1.calculate_total_price())  # 200000
-    print(item2.calculate_total_price())  # 100000
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
 
+def test_item():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+def test_change_rate():
     # устанавливаем новый уровень цен
     Item.pay_rate = 0.8
     # применяем скидку
     item1.apply_discount()
 
-    print(item1.price)  # 8000.0
-    print(item2.price)  # 20000
+    assert item1.price == 8000.0
+    assert item2.price == 20000
 
-    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
