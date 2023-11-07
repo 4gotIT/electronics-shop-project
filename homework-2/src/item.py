466c934a2041ffd0f2ca6c19dc8f50f41a7f3054
Aziz <ibodov.aziz@yandex.ru>
2023-11-07 13:12:54 +0300

    add @property setter

---
+++ homework-2/src/item.py
@@ -0,0 +1,44 @@
+class Item:
+    """
+    Класс для представления товара в магазине.
+    """
+    pay_rate = 1.0
+    all = []
+
+    def __init__(self, name: str, price: float, quantity: int) -> None:
+        """
+        Создание экземпляра класса item.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        """
+        self._name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            self._name = new_name[:10]
+        else:
+            self._name = new_name
+
+    def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+
+        :return: Общая стоимость товара.
+        """
+        return self.price * self.quantity
+
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
+        self.price *= self.pay_rate
