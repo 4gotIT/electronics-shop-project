ee17e60343be89cfad1d52c93fa46bd5f42295a7
Aziz <ibodov.aziz@yandex.ru>
2023-11-10 16:41:51 +0300

    fix class

--- src/phone.py
+++ src/phone.py
@@ -1,12 +1,15 @@
-from item import Item
+from src.item import Item
 
 class Phone(Item):
-    def __init__(self, name, price, quantity, supported_sim_cards):
-        super().__init__(name, price, quantity)
-        self.supported_sim_cards = supported_sim_cards
+    """Класс телефон"""
 
-    def __add__(self, other):
-        if isinstance(other, Phone):
-            self.quantity += other.quantity
-            return Phone(self.name, self.price, self.quantity, self.supported_sim_cards)
-        raise TypeError(f'Нельзя сложить экземпляр {self.__class__} с другим об')
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        if number_of_sim > 0:
+            self.number_of_sim = number_of_sim
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self._name}, {self.price}, {self.quantity}, {self.number_of_sim})'
+
