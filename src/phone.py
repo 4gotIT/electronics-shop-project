6a17deda31f98e9c1c04b989f58af82f9fd83790
Aziz <ibodov.aziz@yandex.ru>
2023-11-16 14:06:31 +0300

    fix 4 task

--- src/phone.py
+++ src/phone.py
@@ -5,11 +5,33 @@ class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        if number_of_sim > 0:
-            self.number_of_sim = number_of_sim
+        self.__nuber_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__nuber_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value <= 0:
+            raise ValueError('Количество сим не может быть меньше или равно нулю')
         else:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+            self.__nuber_of_sim = value
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({self._name}, {self.price}, {self.quantity}, {self.number_of_sim})'
+        return f"{self.__class__.__name__}('{self._name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
+if __name__ == '__main__':
+
+    # смартфон iPhone 14, цена 120_000, количество товара 5, симкарт 2
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert str(phone1) == 'iPhone 14'
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+    assert phone1.number_of_sim == 2
+
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+
+    phone1.number_of_sim = 0
+    # ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -4,9 +4,10 @@ from src.phone import Item
 
 
 def test_phone_class():
+    # смартфон iPhone 14, цена 120_000, количество товара 5, симкарт 2
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
-    assert repr(phone1) == "Phone(iPhone 14, 120000, 5, 2)"
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
     assert phone1.number_of_sim == 2
 
     item1 = Item("Смартфон", 10000, 20)
